79f99dd9807b8ca9940c2a5e1945120cb833611a
YugChauhan <136249547+YugChauhan@users.noreply.github.com>
2023-09-24 12:42:21 +0000

    added flask project

--- app.py
+++ app.py
@@ -2,8 +2,27 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+Jobs=[
+  {
+    "title":"Data Scientist",
+    "location":"India, U.P.",
+    "salary":"$20000"
+  },
+  {
+    "title":"Data Analyst",
+    "location":"India, M.P.",
+    "salary":"$10000"
+  },
+  {
+    "title":"Software Engineer",
+    "location":"India, Karnataka",
+    "salary":"$14000"
+  }
+]
+
+
 @app.route("/")
 def index_html():
-    return render_template("index.html")
+    return render_template("index.html",jobs=Jobs)
 
 app.run(host='0.0.0.0',debug=True)
